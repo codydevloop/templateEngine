b8810504d65a911718925cd9461feb4c0fb00452
CodyHoover <codydevloop@gmail.com>
2020-05-10 16:51:38 -0700

    all tests passing

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -5,7 +5,7 @@ const Employee = require("./Employee.js");
 
 class Engineer extends Employee {
     constructor(name, id, email, github){
-        super(name, id, email)
+        super(name, id, email);
         this.github = github;       
     };
 
