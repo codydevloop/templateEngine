9b08beff79f51c63a0fced8b1c63e547f10aaa9a
CodyHoover <codydevloop@gmail.com>
2020-05-10 16:39:10 -0700

    all test passing on engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Employee class
+
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        super(name, id, email)
+        this.github = github;       
+    };
+
+    getGitHub(){
+        return this.github;
+    }
+
+    getRole(){
+        return "Engineer";
+    };
+}
+
+module.exports = Engineer;
